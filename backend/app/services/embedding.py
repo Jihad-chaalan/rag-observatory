d898f3554fb6312c7864edb5fa11bd00ade34d0c
Jihad Chaalan <chaalan2004@gmail.com>
2026-05-24 15:13:09 +0300

    change embedding

--- backend/app/services/embedding.py
+++ backend/app/services/embedding.py
@@ -3,7 +3,6 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 
 _embeddings = HuggingFaceEmbeddings(
     model_name="all-MiniLM-L6-v2",
-    model_kwargs={'device': 'cpu'},
     encode_kwargs={'normalize_embeddings': True}
 )
 
